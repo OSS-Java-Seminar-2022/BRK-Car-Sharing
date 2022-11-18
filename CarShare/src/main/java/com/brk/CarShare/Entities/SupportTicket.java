09897e590c71f9aee64da8cfce4003acb498a124
bb00l3 <brunobule1969@gmail.com>
2022-11-18 13:46:11 +0100

    SEM - fixed supportTicket document collection name

--- CarShare/src/main/java/com/brk/CarShare/Entities/SupportTicket.java
+++ CarShare/src/main/java/com/brk/CarShare/Entities/SupportTicket.java
@@ -9,7 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Document(collection = "role")
+@Document(collection = "supportTicket")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
