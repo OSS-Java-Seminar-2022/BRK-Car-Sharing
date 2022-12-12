1068d323b9a351c00e7d063bb61d8f326a89a5ef
reactst <reactst@gmail.com>
2022-12-12 13:08:15 +0100

    User CRUD ready for pull request

--- CarShare/src/main/java/com/brk/CarShare/Controllers/UserController.java
+++ CarShare/src/main/java/com/brk/CarShare/Controllers/UserController.java
@@ -1,4 +1,39 @@
 package com.brk.CarShare.Controllers;
 
+import com.brk.CarShare.Entities.User;
+import com.brk.CarShare.Services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+    @Autowired
+    private final UserService userService;
+    public ResponseEntity addUser(@RequestBody User user){
+        userService.addUser(user);
+        return ResponseEntity.ok("Added user");
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {return ResponseEntity.ok(userService.getAllUsers());}
+    @GetMapping("/{userId}")
+    public ResponseEntity getUserById(@PathVariable String id){
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+    @PatchMapping("/{userId}")
+    public ResponseEntity updateUser (@RequestBody User user){
+        userService.updateUser(user);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+    @DeleteMapping("/{userId}")
+    public ResponseEntity deleteUser(@PathVariable String id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- CarShare/src/main/java/com/brk/CarShare/Services/UserService.java
+++ CarShare/src/main/java/com/brk/CarShare/Services/UserService.java
@@ -18,5 +18,20 @@ public class UserService {
     public User getUserById(String id){
         return userRepository.findById(id).orElseThrow(()->new RuntimeException(String.format("Cannot Find User by ID - %s", id)));
     }
+    public void updateUser(User user){
+        User savedUser = userRepository.findById(user.getUserID()).orElseThrow(() -> new RuntimeException(String.format("Cannot Find Vehicle by ID %s", user.getUserID())));
+        savedUser.setUserID(user.getUserID());
+        savedUser.setRoleID(user.getRoleID());
+        savedUser.setDriverLicenceNo(user.getDriverLicenceNo());
+        savedUser.setFirstName(user.getFirstName());
+        savedUser.setLastName(user.getLastName());
+        savedUser.setCountry(user.getCountry());
+        savedUser.setCity(user.getCity());
+        savedUser.setAddress(user.getAddress());
+        savedUser.setPhoneNo(user.getPhoneNo());
+        savedUser.setJoinDate(user.getJoinDate());
+        savedUser.setSubscriptionTier(user.getSubscriptionTier());
+        savedUser.setBillingCycle(user.getBillingCycle());
+    }
     public void deleteUser(String userId) {userRepository.deleteById(userId);}
 }
